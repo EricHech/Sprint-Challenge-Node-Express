4dc5e93902cc104951d40db4ba3f3280ae90ac68
temp <temp@Debis-Home-Mac.local>
2018-03-09 13:06:05 -0500

    Minor improvements to response, and added an error catch-all for the router.

--- src/controllers/BITcontroller.js
+++ src/controllers/BITcontroller.js
@@ -6,7 +6,9 @@ const { getCurrentPrice, getHistoricalPrice } = require('../models/BITmodel');
 const STATUS_USER_ERROR = 422;
 
 let currentPrice = 0;
+let currentPriceTime;
 let previousPrice = 0;
+let previousPriceTime;
 let differenceBetweenValues = 0;
 
 router.get('/compare', (req, res) => {
@@ -14,18 +16,40 @@ router.get('/compare', (req, res) => {
     getCurrentPrice()
       .then(data => {
         currentPrice = data.bpi.USD.rate_float;
-        res.send({ currentDayValue: currentPrice });
+        currentPriceTime = data.time.updated;
+        res.send({
+          currentDayInformation: {
+            currentDayValue: currentPrice,
+            timeOfValue: currentPriceTime,
+          },
+        });
       })
       .catch(err => console.log('There was an error: ', err));
   } else {
     getHistoricalPrice()
       .then(data => {
         previousPrice = Object.values(data.bpi)[0];
+        previousPriceTime = data.time.updated;
         differenceBetweenValues = currentPrice - previousPrice;
-        res.send({ previousDayValue: previousPrice, differenceBetweenValues: differenceBetweenValues });
+        res.send({
+          previousDayInformation: {
+            previousDayValue: previousPrice,
+            timeOfValue: previousPriceTime,
+          },
+          currentDayInformation: {
+            currentDayValue: currentPrice,
+            timeOfValue: currentPriceTime,
+          },
+          priceDifference: differenceBetweenValues,
+        });
       })
       .catch(err => console.log('There was an error: ', err));
   }
+});
+
+router.get('*', (req, res) => {
+  res.status(STATUS_USER_ERROR);
+  res.send("Page not found");
 });
 
 module.exports = router;
